7255bd08f36669a4bc8cf37ea1ffd53c7006ce5f
Juan Urena Garcia Gasco <jurenag@alpha30.aulas.gsyc.urjc.es>
2018-04-05 16:38:39 +0200

    Primera accion

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from cms.models import Pages
 
 # Create your views here.
+
+def menu(request):
+    return HttpResponse("Bienvenido al menu")
+    
+def page(request, name):
+    try:
+        data = Pages.objects.get(name=name)
+        return HttpResponse(data.page)
+    except Pages.DoesNotExist:
+        return HttpResponse("Bienvenido a "+ name +" la pagina no existe")
+
